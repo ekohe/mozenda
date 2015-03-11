232436c20272da0538410cbca4931026fa1e36b7
Piotr Woloszun <piotr.woloszun@gmail.com>
2015-03-11 22:35:06 +0100

    fixed Agent#get_jobs bug

--- lib/mozenda/response/agent_get_jobs.rb
+++ lib/mozenda/response/agent_get_jobs.rb
@@ -2,7 +2,7 @@ module Mozenda::Response
   class AgentGetJobs < Mozenda::Response::Base
 
     def job_list
-      body_hash["JobList"]["Job"]
+      body_hash["JobList"]["Job"] rescue []
     end
 
     private
