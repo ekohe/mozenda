f303287dcb3957a4b94b2a14a6b76f4c9861ab43
Piotr Woloszun <piotr.woloszun@gmail.com>
2015-03-11 23:48:27 +0100

    fixed Agent#get_jobs bug

--- lib/mozenda/response/agent_get_jobs.rb
+++ lib/mozenda/response/agent_get_jobs.rb
@@ -2,7 +2,8 @@ module Mozenda::Response
   class AgentGetJobs < Mozenda::Response::Base
 
     def job_list
-      Array.wrap(body_hash["JobList"]["Job"] rescue [])
+      result = body_hash["JobList"]["Job"] rescue []
+      Array.wrap(result)
     end
 
     private
